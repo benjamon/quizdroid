65e5036da22551a830cfd5684ec35afc45d25b38
benjamon <benjampeterson@gmail.com>
2015-05-02 17:51:46 -0700

    finished up with plenty of time to spare

--- app/src/main/java/edu/washington/benjamon/quizdroid/TopicPageActivity.java
+++ app/src/main/java/edu/washington/benjamon/quizdroid/TopicPageActivity.java
@@ -20,11 +20,20 @@ public class TopicPageActivity extends ActionBarActivity {
         setContentView(R.layout.topic_page);
 
         TextView tenxt = (TextView)findViewById(R.id.topictitle);
+        TextView desxt = (TextView)findViewById(R.id.descriptiontext);
         Button nexet = (Button)findViewById(R.id.nextbut);
 
         Intent intertent = getIntent();
         String titil = intertent.getStringExtra("topic");
         toptit = titil;
+
+        if (toptit.equals("Math")) {
+            desxt.setText(R.string.mathd);
+        } else if (toptit.equals("Physics")) {
+            desxt.setText(R.string.physicsd);
+        } else if (toptit.equals("Marvel Super Heroes")) {
+            desxt.setText(R.string.marveld);
+        }
 
         tenxt.setText(titil);
 
